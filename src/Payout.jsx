c925011c084b94c6e91b1f8f3fe6dfa4e1dda782
Marko Dzúrik <marko.dzurik@gmail.com>
2020-11-17 20:00:46 +0100

    Package init

---
+++ src/Payout.jsx
@@ -0,0 +1,27 @@
+import React, {useEffect} from 'react'
+import { WebView } from 'react-native-webview'
+
+const PayoutPayment = ({ checkoutUrl = '', baseUrl = 'https://payout.one',  uiSettings = { } }) => {
+
+	const [paymentForm, setPaymentForm] = useState(null)
+
+	useEffect(() => {
+		fetch(
+			checkoutUrl,
+			{
+				method: 'POST',
+				headers: {
+					Accept: '*/*'
+				},
+				body: JSON.stringify(uiSettings)
+			}
+		)
+			.then((response) => setPaymentForm(response))
+			.catch((error) => console.error(error))
+	}, [])
+
+	return (
+		<WebView source={ { html: paymentForm, baseUrl: baseUrl } } />
+	)
+}
+export default PayoutPayment
