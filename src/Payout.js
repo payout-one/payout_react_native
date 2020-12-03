8dc8cc77037378649493d5055fd3c99068dd02a5
Marko Dzúrik <marko.dzurik@gmail.com>
2020-12-03 17:35:10 +0100

    Compiled SDK component

---
+++ src/Payout.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { WebView } from 'react-native-webview';
+export const PayoutPayment = ({ checkoutUrl, uiSettings }) => {
+    const [uri, setUri] = useState(null);
+    useEffect(() => {
+        const enhancedUri = processUiSettings(uiSettings);
+        setUri((checkoutUrl + enhancedUri));
+    }, []);
+    const processUiSettings = (uiSettings) => {
+        if (Object.keys(uiSettings).length === 0) {
+            return '';
+        }
+        let index = 0;
+        let params = '?';
+        for (let key in uiSettings) {
+            if (uiSettings.hasOwnProperty(key)) {
+                index += 1;
+                params = params + key + '=' + encodeURIComponent(uiSettings[key]);
+                if (index !== Object.keys(uiSettings).length) {
+                    params = params + '&';
+                }
+            }
+        }
+        return params;
+    };
+    return (React.createElement(WebView, { source: { uri: uri } }));
+};
