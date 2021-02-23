7cdfc9bca60bad881d1337d6f5a25ca1da54172e
Marko Dzúrik <marko.dzurik@gmail.com>
2021-02-23 16:31:14 +0100

    Added optional props types

--- src/Payout.tsx
+++ src/Payout.tsx
@@ -1,39 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import { WebView, WebViewProps } from 'react-native-webview'
 
-type UiSettings = {
-	color: string,
-	anchor_color: string,
-	background_color: string,
-	form_background_color: string,
-	form_border_radius: number,
-	form_border_color: string,
-	form_border_size: number,
-	form_no_margin: boolean
-	remove_header: boolean,
-	remove_footer: boolean,
-	show_card_payment: boolean
-	show_online_payment: boolean,
-	show_bank_payment: boolean,
-	button_color: string,
-	button_background_color: string,
-	button_border_size: number,
-	button_border_color: string,
-	button_border_radius: number,
-	button_hover_background_color: string,
-	button_hover_border_color: string,
-	button_hover_color: string,
-	input_color: string,
-	input_background_color: string,
-	input_border_radius: number,
-	input_border_color: string,
-	input_border_size: number
+export type UiSettings = {
+	color?: string,
+	anchor_color?: string,
+	background_color?: string,
+	form_background_color?: string,
+	form_border_radius?: number,
+	form_border_color?: string,
+	form_border_size?: number,
+	form_no_margin?: boolean
+	remove_header?: boolean,
+	remove_footer?: boolean,
+	show_card_payment?: boolean
+	show_online_payment?: boolean,
+	show_bank_payment?: boolean,
+	button_color?: string,
+	button_background_color?: string,
+	button_border_size?: number,
+	button_border_color?: string,
+	button_border_radius?: number,
+	button_hover_background_color?: string,
+	button_hover_border_color?: string,
+	button_hover_color?: string,
+	input_color?: string,
+	input_background_color?: string,
+	input_border_radius?: number,
+	input_border_color?: string,
+	input_border_size?: number
 }
 
 type Props = {
 	checkoutUrl: string,
-	uiSettings: UiSettings,
-	props: WebViewProps,
+	uiSettings?: UiSettings,
+	props?: WebViewProps,
 }
 
 export const PayoutPayment: React.FC<Props> = ({checkoutUrl, uiSettings, props}) => {
