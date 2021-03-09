4062603b2568f4c230801c7874f2c41e1e5bf3ef
Marko Dzúrik <marko.dzurik@gmail.com>
2021-03-09 16:18:30 +0100

    Added onMessage data validation

--- src/Payout.tsx
+++ src/Payout.tsx
@@ -1,82 +1,80 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import {WebView, WebViewMessageEvent, WebViewProps} from 'react-native-webview'
 
 export type UiSettings = {
-	color?: string,
-	anchor_color?: string,
-	background_color?: string,
-	form_background_color?: string,
-	form_border_radius?: number,
-	form_border_color?: string,
-	form_border_size?: number,
-	form_no_margin?: boolean
-	remove_header?: boolean,
-	remove_footer?: boolean,
-	show_card_payment?: boolean
-	show_online_payment?: boolean,
-	show_bank_payment?: boolean,
-	button_color?: string,
-	button_background_color?: string,
-	button_border_size?: number,
-	button_border_color?: string,
-	button_border_radius?: number,
-	button_hover_background_color?: string,
-	button_hover_border_color?: string,
-	button_hover_color?: string,
-	input_color?: string,
-	input_background_color?: string,
-	input_border_radius?: number,
-	input_border_color?: string,
-	input_border_size?: number
+    color?: string,
+    anchor_color?: string,
+    background_color?: string,
+    form_background_color?: string,
+    form_border_radius?: number,
+    form_border_color?: string,
+    form_border_size?: number,
+    form_no_margin?: boolean
+    remove_header?: boolean,
+    remove_footer?: boolean,
+    show_card_payment?: boolean
+    show_online_payment?: boolean,
+    show_bank_payment?: boolean,
+    button_color?: string,
+    button_background_color?: string,
+    button_border_size?: number,
+    button_border_color?: string,
+    button_border_radius?: number,
+    button_hover_background_color?: string,
+    button_hover_border_color?: string,
+    button_hover_color?: string,
+    input_color?: string,
+    input_background_color?: string,
+    input_border_radius?: number,
+    input_border_color?: string,
+    input_border_size?: number
 }
 
 type Props = {
-	checkoutUrl: string,
-	onClose?: () => void,
-	uiSettings?: UiSettings,
-	props?: WebViewProps,
+    checkoutUrl: string,
+    onClose?: () => void,
+    uiSettings?: UiSettings,
+    props?: WebViewProps,
 }
 
 export const PayoutPayment: React.FC<Props> = ({checkoutUrl, uiSettings, props, onClose}) => {
 
-	const [uri, setUri] = useState(checkoutUrl)
+    const [uri, setUri] = useState(checkoutUrl)
 
-	useEffect(()=>{
-		const enhancedUri = processUiSettings(uiSettings)
-		setUri(checkoutUrl + enhancedUri)
-	},[])
+    useEffect(() => {
+        const enhancedUri = processUiSettings(uiSettings)
+        setUri(checkoutUrl + enhancedUri)
+    }, [])
 
-	const processUiSettings = (uiSettings: any) => {
-		if (Object.keys(uiSettings).length === 0) {
-			return ''
-		}
-		let index = 0
-		let params = '?'
-		for (let key in uiSettings) {
-			if (uiSettings.hasOwnProperty(key)) {
-				index += 1
-				params = params + key + '=' + encodeURIComponent(uiSettings[key])
-				if (index !== Object.keys(uiSettings).length) {
-					params = params + '&'
-				}
-			}
-		}
+    const processUiSettings = (uiSettings: any) => {
+        if (Object.keys(uiSettings).length === 0) {
+            return ''
+        }
+        let index = 0
+        let params = '?'
+        for (let key in uiSettings) {
+            if (uiSettings.hasOwnProperty(key)) {
+                index += 1
+                params = params + key + '=' + encodeURIComponent(uiSettings[key])
+                if (index !== Object.keys(uiSettings).length) {
+                    params = params + '&'
+                }
+            }
+        }
 
-		return params
-	}
+        return params
+    }
 
-	const onMessageHandler = (event: WebViewMessageEvent) => {
-		if(props?.onMessage){
-			props.onMessage(event)
-		} else {
-			let message  = JSON.parse(event.nativeEvent.data);
-			if(message.action && message.action === 'CLOSE') {
-				onClose && onClose()
-			}
-		}
-	}
+    const onMessageHandler = (event: WebViewMessageEvent) => {
+        if(event && event.nativeEvent && event.nativeEvent.data){
+            let message = JSON.parse(event.nativeEvent.data);
+            if (message.action && message.action === 'CLOSE') {
+                onClose && onClose()
+            }
+        }
+    }
 
-	return (
-		<WebView source={ { uri } } onMessage={(data) => onMessageHandler(data)} {...props} />
-	)
+    return (
+        <WebView source={{uri}} onMessage={onMessageHandler} {...props} />
+    )
 }
